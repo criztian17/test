9a2841abe281a19ccf0b4b996cba5bf7abfd8c63
Cristian Gutierrez <cristian.gutierrez@realpage.com>
2020-08-08 13:45:30 -0500

    Repository Pattern

---
+++ test.Backend/test.Repository/Repositories/Implementations/GenericRepository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using test.Repository.Entities;
+using test.Repository.Repositories.Interfaces;
+
+namespace test.Repository.Repositories.Implementations
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class, IEntity
+    {
+        #region Attributes
+        private readonly DataContext context;
+        #endregion
+
+        #region Public Methods
+        public GenericRepository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return this.context.Set<T>().AsNoTracking();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await this.context.Set<T>()
+                .AsNoTracking()
+                .FirstOrDefaultAsync(e => e.Id == id);
+        }
+
+        public async Task CreateAsync(T entity)
+        {
+            await this.context.Set<T>().AddAsync(entity);
+            await SaveAllAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            this.context.Set<T>().Update(entity);
+            await SaveAllAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            this.context.Set<T>().Remove(entity);
+            await SaveAllAsync();
+        }
+
+        public async Task<bool> ExistAsync(int id)
+        {
+            return await this.context.Set<T>().AnyAsync(e => e.Id == id);
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await this.context.SaveChangesAsync() > 0;
+        } 
+        #endregion
+    }
+}
