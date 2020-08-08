2592de89ca83394f2084bfe1c20f3f38364bad2c
Cristian Gutierrez <cristian.gutierrez@realpage.com>
2020-08-07 19:50:50 -0500

    Wrappers

---
+++ test.Backend/test.Utilities/Common/CommonUtilities.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace test.Utilities.Common
+{
+    public static class CommonUtilities
+    {
+        #region Public Methods
+        /// <summary>
+        /// Get the name of the executed method
+        /// </summary>
+        /// <param name="classType">Type of the class</param>
+        /// <returns>The name of the method</returns>
+        public static string GetExecutedMethod(Type classType)
+        {
+            try
+            {
+                string resultValue = string.Concat((classType == null ? string.Empty : classType.FullName));
+                StackTrace trace = new StackTrace(false);
+
+                bool isTheMethod = false;
+
+                foreach (StackFrame frame in trace.GetFrames())
+                {
+                    MethodBase method = frame.GetMethod();
+
+                    if (classType != null)
+                    {
+                        isTheMethod = (method.DeclaringType == classType && classType.IsAssignableFrom(method.DeclaringType)) ? true : false;
+                    }
+
+                    if (isTheMethod)
+                    {
+                        resultValue = string.Concat(method.DeclaringType.FullName, ".", method.Name, "()");
+
+                        break;
+                    }
+                }
+
+                return resultValue;
+            }
+            catch (Exception)
+            {
+                return "UnknowMethod()";
+            }
+        }
+
+        /// <summary>
+        /// Get the name of the executed method in an async call
+        /// </summary>
+        /// <param name="classType">Type of class</param>
+        /// <returns>The name of the method</returns>
+        public static string GetParallelExecutedMethod(Type classType)
+        {
+            try
+            {
+                string resultValue = string.Concat((classType == null ? string.Empty : classType.FullName));
+                StackTrace trace = new StackTrace(false);
+
+                foreach (StackFrame frame in trace.GetFrames())
+                {
+                    MethodBase method = frame.GetMethod();
+
+                    if (method.DeclaringType.FullName.Contains(classType.FullName))
+                    {
+                        resultValue = string.Concat(method.DeclaringType.FullName.ExtractNameExecutedMethod(), "()");
+                        break;
+                    }
+                }
+
+                return resultValue;
+            }
+            catch (Exception)
+            {
+                return "UnknowMethod()";
+            }
+        } 
+        #endregion
+
+        #region "Private Methods"
+
+        /// <summary>
+        /// Get the name of the method from a string value
+        /// </summary>
+        /// <param name="value">Value to find</param>
+        /// <returns>The name of the method</returns>
+        private static string ExtractNameExecutedMethod(this string value)
+        {
+            int initialPosition = value.IndexOf("<<") + 1;
+            int finalPosition = value.IndexOf(">", value.IndexOf(">") + 1);
+
+            return value.Substring(initialPosition + 1, finalPosition - initialPosition);
+        }
+
+        #endregion 
+    }
+}
---
+++ test.Backend/test.Utilities/Extensions/ExecutionWrapperExtension.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Threading.Tasks;
+using test.Utilities.ApiExceptions;
+using test.Utilities.Common;
+
+namespace test.Utilities.Extensions
+{
+    /// <summary>
+    /// Wrapper class that encapsulate the execution of functions
+    /// </summary>
+    public class ExecutionWrapperExtension
+    {
+        #region "Public Methods"
+
+        /// <summary>
+        /// Wrapper for encapsulating the execution of functions and handle the exceptions
+        /// </summary>
+        /// <typeparam name="T">Return type</typeparam>
+        /// <typeparam name="C">Method Class</typeparam>
+        /// <param name="executionBody">Body to be executed</param>
+        /// <param name="doInError">Do when error occours</param>
+        /// <param name="doInFinally">Do when finally</param>
+        /// <param name="throwException">Indicates if throw an exception or not</param>
+        /// <returns>The function executed</returns>
+        public static T ExecuteWrapper<T, C>(Func<T> executionBody, Func<Exception, T> doInError, Func<T, T> doInFinally, bool throwException = true) where C : class
+        {
+            T returnValue = default;
+            //never used?
+            //Guid correlationId = Guid.NewGuid();
+            try
+            {
+                returnValue = executionBody();
+            }
+            catch (Exception exception)
+            {
+                string methodName = CommonUtilities.GetExecutedMethod(typeof(C));
+
+                exception = exception.InnerException == null ? exception : exception.GetBaseException();
+
+                if (exception is BusinessException == false)
+                {
+                    ///Escribir en serilog
+                }
+
+                if (doInError == null)
+                {
+                    if (exception is BusinessException)
+                    {
+                        BusinessException businessException = (BusinessException)exception;
+
+                        if (throwException)
+                        {
+                            throw new BusinessException(businessException.StatusCode, businessException.Result);
+                        }
+                    }
+                    else
+                    {
+                        if (throwException)
+                        {
+                            throw new Exception(exception.Message);
+                        }
+                    }
+                }
+                else
+                {
+                    returnValue = doInError(exception);
+                }
+            }
+
+            finally
+            {
+                if (doInFinally != null)
+                {
+                    returnValue = doInFinally(returnValue);
+                }
+            }
+
+            return returnValue;
+        }
+
+        /// <summary>
+        /// Wrapper for encapsulating the execution of functions and handle the exceptions
+        /// </summary>
+        /// <typeparam name="T">Return type</typeparam>
+        /// <typeparam name="C">Method Class</typeparam>
+        /// <param name="executionBody">Body to be executed</param>
+        /// <param name="doInError">Do when error occours</param>
+        /// <returns>The function executed</returns>
+        public static T ExecuteWrapper<T, C>(Func<T> executionBody, Func<Exception, T> doInError) where C : class
+        {
+            return ExecuteWrapper<T, C>(executionBody, doInError, null);
+        }
+
+        /// <summary>
+        /// Wrapper for encapsulating the execution of functions and handle the exceptions
+        /// </summary>
+        /// <typeparam name="T">Return type</typeparam>
+        /// <typeparam name="C">Method Class</typeparam>
+        /// <param name="executionBody">Body to be executed</param>
+        /// <returns>The function executed</returns>
+        public static T ExecuteWrapper<T, C>(Func<T> executionBody) where C : class
+        {
+            return ExecuteWrapper<T, C>(executionBody, null, null);
+        }
+
+        /// <summary>
+        /// Wrapper for encapsulating the execution of functions and handle the exceptions
+        /// </summary>
+        /// <typeparam name="C">Method Class</typeparam>
+        /// <param name="executionBody">Body to be executed</param>
+        public static void ExecuteWrapper<C>(Action executionBody) where C : class
+        {
+            bool nada = ExecuteWrapper<bool, C>(() =>
+            {
+                executionBody();
+                return false;
+            });
+        }
+
+        #endregion
+
+        #region "Public Async Methods"
+
+        /// <summary>
+        /// Wrapper for encapsulating the execution of functions and handle the exceptions
+        /// </summary>
+        /// <typeparam name="T">Return type</typeparam>
+        /// <typeparam name="C">Method Class</typeparam>
+        /// <param name="executionBody">Body to be executed</param>
+        /// <param name="doInError">Do when error occours</param>
+        /// <param name="doInFinally">Do when finally</param>
+        /// <param name="throwException">Indicates if throw an exception or not</param>
+        /// <returns>The function executed</returns>
+        public static async Task<T> ExecuteWrapperAsync<T, C>(Func<Task<T>> executionBody, Func<Exception, T> doInError, Func<T, T> doInFinally, bool throwException = true) where C : class
+        {
+            T returnValue = default(T);
+            Guid correlationId = Guid.NewGuid();
+            try
+            {
+                returnValue = await executionBody();
+            }
+            catch (Exception exception)
+            {
+                string methodName = CommonUtilities.GetParallelExecutedMethod(typeof(C));
+                exception = exception.InnerException == null ? exception : exception.GetBaseException();
+
+                if (exception is BusinessException == false)
+                {
+                    ///Escribir en serilog
+                }
+
+                if (doInError == null)
+                {
+                    if (exception is BusinessException)
+                    {
+                        BusinessException businessException = (BusinessException)exception;
+
+                        if (throwException)
+                        {
+                            throw new BusinessException(businessException.StatusCode, businessException.Result);
+                        }
+                    }
+                    else
+                    {
+                        if (throwException)
+                        {
+                            throw new Exception(exception.Message);
+                        }
+                    }
+                }
+                else
+                {
+                    returnValue = doInError(exception);
+                }
+            }
+
+            finally
+            {
+                if (doInFinally != null)
+                {
+                    returnValue = doInFinally(returnValue);
+                }
+            }
+
+            return returnValue;
+        }
+
+        /// <summary>
+        /// Wrapper for encapsulating the execution of functions and handle the exceptions
+        /// </summary>
+        /// <typeparam name="T">Return type</typeparam>
+        /// <typeparam name="C">Method Class</typeparam>
+        /// <param name="executionBody">Body to be executed</param>
+        /// <param name="doInError">Do when error occours</param>
+        /// <returns>The function executed</returns>
+        public static async Task<T> ExecuteWrapperAsync<T, C>(Func<Task<T>> executionBody, Func<Exception, T> doInError) where C : class
+        {
+            return await ExecuteWrapperAsync<T, C>(executionBody, doInError, null);
+        }
+
+        /// <summary>
+        /// Wrapper for encapsulating the execution of functions and handle the exceptions
+        /// </summary>
+        /// <typeparam name="T">Return type</typeparam>
+        /// <typeparam name="C">Method Class</typeparam>
+        /// <param name="executionBody">Body to be executed</param>
+        /// <returns>The function executed</returns>
+        public static async Task<T> ExecuteWrapperAsync<T, C>(Func<Task<T>> executionBody) where C : class
+        {
+            return await ExecuteWrapperAsync<T, C>(executionBody, null, null);
+        }
+
+        #endregion
+    }
+}
