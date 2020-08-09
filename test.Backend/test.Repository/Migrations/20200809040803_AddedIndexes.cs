10ab5031475564f8a72ccca9bfceb98f7b4367c9
Cristian Gutierrez <cristian.gutierrez@realpage.com>
2020-08-08 23:09:22 -0500

    Indexes Migration

---
+++ test.Backend/test.Repository/Migrations/20200809040803_AddedIndexes.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace test.Repository.Migrations
+{
+    public partial class AddedIndexes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Coverages_Description",
+                table: "Coverages",
+                column: "Description");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_Identification",
+                table: "Clients",
+                column: "Identification");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Coverages_Description",
+                table: "Coverages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Clients_Identification",
+                table: "Clients");
+        }
+    }
+}
